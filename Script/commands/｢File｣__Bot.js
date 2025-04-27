255504192d746b5c0a091a45e66f9676d1703ded
arata-rohit-bot-exe <aratakaizakibotexe@gmail.com>
2025-04-27 15:23:44 +0600

    Update ｢File｣__Bot.js

--- Script/commands/｢File｣__Bot.js
+++ Script/commands/｢File｣__Bot.js
@@ -91,7 +91,7 @@ module.exports.handleEvent = async function({ api, event, args, Threads, Users }
      return api.sendMessage("সরি বস মাফ করে দেন আর এমন ভুল হবে না🥺🙏", threadID);
    };
 
-   if ((event.body.toLowerCase() == "@Farhana Ontora") || (event.body.toLowerCase() == " ")) {
+   if ((event.body.toLowerCase() == "@Farhana Ontora") || (event.body.toLowerCase() == "riya")) {
      return api.sendMessage("খবরদার কেউ এই আইড়ি মেনশন দিবানা এটা আমার বস উল্লাস এর বউ এর আইড়ি😠🥰⛏️", threadID);
    };
 
@@ -99,7 +99,7 @@ module.exports.handleEvent = async function({ api, event, args, Threads, Users }
      return api.sendMessage("খবরদার কেউ এই নাম দরে ডাক দিবানা এটা আমার বস রহিত এর বউ এর নাম..!😠🥰⛏️", threadID);
    };
   
-  if ((event.body.toLowerCase() == "") || (event.body.toLowerCase() == "")) {
+  if ((event.body.toLowerCase() == "sadiya") || (event.body.toLowerCase() == "mim")) {
      return api.sendMessage("খবরদার কেউ এই নাম দরে ডাক দিবানা এটা আমার বস রহিত এর বউ এর নাম..!😠🥰⛏️", threadID);
    };
 
